4d84b8175f786ba0f6ca21c5f4449f4e0306fbc2
zerosum99 <myjlms99@gmail.com>
2017-07-05 17:17:24 +0900

    22

---
+++ arguments.py
@@ -0,0 +1,12 @@
+import sys
+
+# it's easy to print this list of course:
+print(sys.argv)
+
+# or it can be iterated via a for loop:
+
+for i in range(len(sys.argv)):
+    if i == 0:
+        print("Function name: %s" % sys.argv[0])
+    else:
+        print("%d. argument: %s" % (i,sys.argv[i]))
---
+++ argv-list.py
@@ -0,0 +1,3 @@
+
+import sys
+print('sys.argv is', sys.argv)
